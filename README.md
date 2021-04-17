32f929fff6d20a94742f4a50332bae37e3365537
Samuel <sagarma_94@hotmail.com>
2021-04-17 21:09:24 +0200

    Parser and events

--- README.md
+++ README.md
@@ -3,3 +3,23 @@ PulseSecure parser for uSIEM
 
 Tested in virtual appliance `9.1R10 (build 10119)` in AWS.
 Easy to deploy, follow https://www-prev.pulsesecure.net/download/techpubs/current/1221/pulse-connect-secure/pcs/9.0rx/ps-pcs-9.0r1-aws-deployment-guide.pdf
+
+# Event ID list
+Login succeded: AUT31504
+Login succeded: ADM22668
+Login failed: AUT23457
+Session timed out: AUT22886
+Primary authentication successful: AUT24326
+Primary authentication successful: AUT30684
+Primary authentication failed: AUT24327
+Password change failed: USR24630
+User account modified: USR22898
+User accounts modified: ADM20716
+Super admin session created using token: ADM23452
+Admin token is created for administrative logon recovery: ADM24511
+Changed log event LicenseServer: ADM20603
+Logon:  AUT24803 
+Update:  AUT23524
+Logout: AUT22673 
+Logout: ADM22671
+Concurrent connection limit: AUT31085
--- src/pulse.rs
+++ src/pulse.rs
@@ -1,26 +1,50 @@
-use chrono::prelude::{DateTime};
+use chrono::prelude::{NaiveDateTime};
 use std::borrow::Cow;
 use usiem::components::common::LogParsingError;
-use usiem::events::field::SiemField;
-use usiem::events::SiemLog;
+use usiem::events::field::{SiemField, SiemIp};
+use usiem::events::{SiemLog, SiemEvent};
+use usiem::events::auth::{AuthEvent,AuthLoginType, LoginOutcome, RemoteLogin};
 use std::collections::BTreeMap;
 
 pub fn parse_general_log(mut log: SiemLog) -> Result<SiemLog, LogParsingError> {
-    let log_line = log.message();
-    let _log_line = match log_line.find(" Query ") {
+    let log_line = log.message().to_string();
+    let log_line = match log_line.find(" id=") {
         None => return Err(LogParsingError::NoValidParser(log)),
-        Some(pos) => {
-            if pos >= 39 {
-                &log_line[pos-37..]
-            }else{
-                log_line
-            }
-        }
+        Some(pos) => &log_line[pos + 1..]
     };
-    log.set_service(Cow::Borrowed("PulseSecure"));
-    log.set_product(Cow::Borrowed("PulseSecure"));
-    log.set_category(Cow::Borrowed("VPN"));
-    return Ok(log)
+    let fields = extract_fields(log_line);
+    
+    let timestamp = match fields.get("time") {
+        Some(timestamp) => {
+            match NaiveDateTime::parse_from_str(*timestamp, "%Y-%m-%d %H:%M:%S") {
+                Ok(timestamp) => timestamp.timestamp_millis(),
+                Err(_err) => return Err(LogParsingError::NoValidParser(log)),
+            }
+        },
+        None => return Err(LogParsingError::NoValidParser(log))
+    };
+    let observer_ip = match fields.get("fw") {
+        Some(fw) => {
+            match SiemIp::from_ip_str(fw) {
+                Ok(ip) => ip,
+                Err(_) => return Err(LogParsingError::ParserError(log)),
+            }
+        },
+        None => return Err(LogParsingError::NoValidParser(log))
+    };
+    let log = parse_msg_field(&fields, log);
+    match log {
+        Ok(mut log) => {
+            log.add_field("observer.ip", SiemField::IP(observer_ip));
+            log.set_event_created(timestamp);
+            log.set_service(Cow::Borrowed("PulseSecure"));
+            log.set_product(Cow::Borrowed("PulseSecure"));
+            log.set_category(Cow::Borrowed("VPN"));
+            return Ok(log)
+        },
+        Err(e) => return Err(e)
+    };
+    
     
 }
 
@@ -72,12 +96,222 @@ pub fn extract_fields<'a>(message: &'a str) -> BTreeMap<&'a str, &'a str> {
     field_map
 }
 
+
+pub fn parse_msg_field(fields : &BTreeMap<&str, &str>, mut log: SiemLog) -> Result<SiemLog, LogParsingError> {
+    let msg = match fields.get("msg") {
+        Some(fw) => *fw,
+        None => return Ok(log)
+    };
+    let (id, content) = match msg.find(": ") {
+        Some(pos) => (&msg[..pos], &msg[pos + 2..]),
+        None => return Err(LogParsingError::ParserError(log))
+    };
+    let typ = &id[0..3];
+    let id = match (&id[3..]).parse::<u32>() {
+        Ok(id) => id,
+        Err(_) => return Err(LogParsingError::ParserError(log))
+    };
+    let user = match fields.get("user") {
+        Some(user) => user,
+        None => ""
+    };
+    let user_domain = match fields.get("realm") {
+        Some(dm) => dm,
+        None => "_NONE_"
+    };
+    let hostname = match fields.get("fw") {
+        Some(user) => user,
+        None => "_NONE_"
+    };
+    let source = match fields.get("src") {
+        Some(src) => *src,
+        None => "_NONE_"
+    };
+    match fields.get("agent") {
+        Some(agnt) => {
+            if agnt.len() > 0{
+                log.add_field("user_agent.original", SiemField::Text(Cow::Owned(agnt.to_string())));
+            }
+        },
+        None => {}
+    };
+    
+    // Add Event ID from msg field and dataset
+    log.add_field("event.code", SiemField::U32(id));
+    log.add_field("event.dataset", SiemField::from_str(typ.to_string()));
+    match typ {
+        "AUT" => {
+            match id {
+                31504 => {
+                    //Login succeded
+                    log.set_event(SiemEvent::Auth(AuthEvent {
+                        hostname : Cow::Owned(hostname.to_string()),
+                        outcome : LoginOutcome::SUCESS,
+                        login_type : AuthLoginType::Remote(RemoteLogin {
+                            user_name : Cow::Owned(user.to_string()),
+                            domain : Cow::Owned(user_domain.to_string()),
+                            source_address : Cow::Owned(source.to_string()),
+                        })
+                    }));
+                },
+                24412 => {
+                    // SOAP login succeeded for
+                    log.set_event(SiemEvent::Auth(AuthEvent {
+                        hostname : Cow::Owned(hostname.to_string()),
+                        outcome : LoginOutcome::SUCESS,
+                        login_type : AuthLoginType::Remote(RemoteLogin {
+                            user_name : Cow::Owned(user.to_string()),
+                            domain : Cow::Owned(user_domain.to_string()),
+                            source_address : Cow::Owned(source.to_string()),
+                        })
+                    }));
+                },
+                24414 => {
+                    // SOAP login succeeded for
+                    log.set_event(SiemEvent::Auth(AuthEvent {
+                        hostname : Cow::Owned(hostname.to_string()),
+                        outcome : LoginOutcome::SUCESS,
+                        login_type : AuthLoginType::Remote(RemoteLogin {
+                            user_name : Cow::Owned(user.to_string()),
+                            domain : Cow::Owned(user_domain.to_string()),
+                            source_address : Cow::Owned(source.to_string()),
+                        })
+                    }));
+                },
+                24326  => {
+                    //Primary authentication successful
+                    log.set_event(SiemEvent::Auth(AuthEvent {
+                        hostname : Cow::Owned(hostname.to_string()),
+                        outcome : LoginOutcome::ESTABLISH,
+                        login_type : AuthLoginType::Remote(RemoteLogin {
+                            user_name : Cow::Owned(user.to_string()),
+                            domain : Cow::Owned(user_domain.to_string()),
+                            source_address : Cow::Owned(source.to_string()),
+                        })
+                    }));
+                },
+                30684 => {
+                    //Primary authentication successful for admin
+                    log.set_event(SiemEvent::Auth(AuthEvent {
+                        hostname : Cow::Owned(hostname.to_string()),
+                        outcome : LoginOutcome::ESTABLISH,
+                        login_type : AuthLoginType::Remote(RemoteLogin {
+                            user_name : Cow::Owned(user.to_string()),
+                            domain : Cow::Owned(user_domain.to_string()),
+                            source_address : Cow::Owned(source.to_string()),
+                        })
+                    }));
+                },
+                24327 => {
+                    //Primary authentication failed
+                    log.set_event(SiemEvent::Auth(AuthEvent {
+                        hostname : Cow::Owned(hostname.to_string()),
+                        outcome : LoginOutcome::FAIL,
+                        login_type : AuthLoginType::Remote(RemoteLogin {
+                            user_name : Cow::Owned(user.to_string()),
+                            domain : Cow::Owned(user_domain.to_string()),
+                            source_address : Cow::Owned(source.to_string()),
+                        })
+                    }));
+                },
+                30685 => {
+                    //Primary authentication failed for admin
+                    log.set_event(SiemEvent::Auth(AuthEvent {
+                        hostname : Cow::Owned(hostname.to_string()),
+                        outcome : LoginOutcome::FAIL,
+                        login_type : AuthLoginType::Remote(RemoteLogin {
+                            user_name : Cow::Owned(user.to_string()),
+                            domain : Cow::Owned(user_domain.to_string()),
+                            source_address : Cow::Owned(source.to_string()),
+                        })
+                    }));
+                },
+                22673 => {
+                    //Logout
+                    
+                },
+                31085 => {
+                    //Concurrent connection limit
+                },
+                _ => {}
+            }
+            return Ok(log)
+        },
+        "USR" => {
+            return Ok(log)
+        },
+        "ADM" => {
+            match id {
+                22668 => {
+                    //Login succeded
+                    log.set_event(SiemEvent::Auth(AuthEvent {
+                        hostname : Cow::Owned(hostname.to_string()),
+                        outcome : LoginOutcome::SUCESS,
+                        login_type : AuthLoginType::Remote(RemoteLogin {
+                            user_name : Cow::Owned(user.to_string()),
+                            domain : Cow::Owned(user_domain.to_string()),
+                            source_address : Cow::Owned(source.to_string()),
+                        })
+                    }));
+                },
+                20716 => {
+                    // User accounts modified
+                },
+                23452 => {
+                    //Super admin session created using token
+                },
+                24511 => {
+                    //Admin token is created
+                },
+                22671 => {
+                    //Logout
+                },
+                _ => {}
+            }
+            return Ok(log)
+        },
+        "PTR" => {
+            // Policy Trace
+            return Ok(log)
+        },
+        "NWC" => {
+            // Network Connect
+            return Ok(log)
+        },
+        "ERR" => {
+            // System Error
+            return Ok(log)
+        },
+        "WEB" => {
+            // WebRequest
+            return Ok(log)
+        },
+        "ARC" => {
+            // Archive
+            return Ok(log)
+        },
+        _ => return Ok(log)
+    }
+}
+
+fn extract_from_msg<'a>(msg : &'a str) -> Option<&'a str> {
+    let pos = match msg.find(" from ") {
+        Some(p) => p,
+        None => return None
+    };
+    match &msg[pos + 6..].find(" ") {
+        Some(p) => return Some(&msg[pos + 6..*p]),
+        None => return Some(&msg[pos + 6..])
+    };
+}
+
 #[cfg(test)]
 mod filterlog_tests {
     use super::{parse_general_log, extract_fields};
     use usiem::events::{SiemLog, SiemEvent};
     use usiem::events::field::{SiemIp, SiemField};
     use usiem::events::auth::{AuthLoginType,LoginOutcome};
+    use std::borrow::Cow;
 
     #[test]
     fn test_extract_fields() {
@@ -89,5 +323,22 @@ mod filterlog_tests {
         assert_eq!(map.get("user"), Some(&"usettest1"));
         assert_eq!(map.get("ivs"), Some(&"Default Network"));
         assert_eq!(map.get("msg"), Some(&"AUT22673: Logout from 82.213.178.130 (session:00000000)"));
+    }
+    #[test]
+    fn test_parser() {
+        let log = "2021-04-08T12:14:18-07:00 10.0.0.111 PulseSecure: id=firewall time=\"2021-04-08 12:14:18\" pri=6 fw=10.0.0.9 vpn=ive ivs=Default Network user=usertest2 realm=\"Users\" roles=\"Users\" proto=auth src=82.213.178.130 dst= dstname= type=vpn op= arg=\"\" result= sent= rcvd= agent=\"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0\" duration= msg=\"AUT31504: Login succeeded for usertest2/Users (session:00000000) from 82.213.178.130 with Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0.\"";
+        let log = SiemLog::new(log.to_string(), 0, SiemIp::V4(0));
+        let res = parse_general_log(log);
+        match res {
+            Ok(log) => {
+                assert_eq!(log.field("user.name"), Some(&SiemField::User(String::from("usertest2"))));
+                assert_eq!(log.field("source.ip"), Some(&SiemField::IP(SiemIp::from_ip_str("82.213.178.130").unwrap())));
+                assert_eq!(log.field("user.domain"), Some(&SiemField::Domain(String::from("Users"))));
+                assert_eq!(log.field("event.code"), Some(&SiemField::U32(31504)));
+                assert_eq!(log.field("observer.ip"), Some(&SiemField::IP(SiemIp::from_ip_str("10.0.0.9").unwrap())));
+                assert_eq!(log.field("user_agent.original"), Some(&SiemField::from_str("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0")));
+            },
+            Err(_) => panic!("Must be parsed")
+        }
     }
 }
